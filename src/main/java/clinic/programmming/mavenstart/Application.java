34aac6062abc012b4f4d1b62026b33747597f684
prince <prince@123.com>
2019-11-19 08:55:30 +0530

    used StringUtils & added dependency to common-langs3

--- src/main/java/clinic/programmming/mavenstart/Application.java
+++ src/main/java/clinic/programmming/mavenstart/Application.java
@@ -1,6 +1,7 @@
 package clinic.programmming.mavenstart;
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 
 public class Application {
@@ -8,7 +9,10 @@ public class Application {
     public Application() {
         System.out.println ("Inside Application");
     }
-	
+	public int countWords(String words){
+		String[] separateWords= StringUtils.split(words,' ');
+		return (separateWords==null) ? 0 : separateWords.length;
+	}
 	public void greet()
 	{
 		List<String> greetings = new ArrayList<>();
@@ -24,5 +28,7 @@ public class Application {
 		System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int appCount = app.countWords("I have complete Maven");
+		System.out.println("count is " + appCount);
     }
 }
